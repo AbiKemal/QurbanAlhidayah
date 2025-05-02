031c90e39a341c645af8b68f92f7c182a7f55da7
AbiKemal <ssupendi@gmail.com>
2025-05-02 22:05:57 +0700

    Update javascript.js

--- script/javascript.js
+++ script/javascript.js
@@ -1,11 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Halaman index.html dimuat");
-
-  fetch("config.json")
-    .then(res => res.json())
-    .then(config => {
-      console.log("Data konfigurasi berhasil dimuat:", config);
-    })
-    .catch(err => console.error("Gagal memuat konfigurasi:", err));
+  google.charts.load('current', {'packages':['table']});
+  google.charts.setOnLoadCallback(loadPesertaList);
 });
 
+function loadPesertaList() {
+  // Ambil config JSON dari tag <script>
+  const configScript = document.getElementById("config-data");
+  const config = JSON.parse(configScript.textContent || '{}');
+  const pesertaConfig = config["PESERTA_QURBAN"];
+
+  if (!pesertaConfig || !gsheet) {
+    document.getElementById("peserta_list").innerHTML = "Konfigurasi tidak ditemukan.";
+    return;
+  }
+
+  const sheetName = pesertaConfig.sheet || "PESERTA_QURBAN";
+  const queryString = encodeURIComponent("SELECT A, B");
+  const dataSourceUrl = `https://docs.google.com/spreadsheets/d/${gsheet}/gviz/tq?sheet=${sheetName}&headers=1&tq=${queryString}`;
+
+  const query = new google.visualization.Query(dataSourceUrl);
+  query.send(function(response) {
+    if (response.isError()) {
+      document.getElementById("peserta_list").innerHTML = "Gagal memuat data peserta.";
+      return;
+    }
+
+    const data = response.getDataTable();
+    let html = '<ul>';
+    for (let i = 0; i < data.getNumberOfRows(); i++) {
+      const nama = data.getValue(i, 1);
+      html += `<li><a href="view.html?nama=${encodeURIComponent(nama)}">${data.getValue(i, 0)} - ${nama}</a></li>`;
+    }
+    html += '</ul>';
+    document.getElementById("peserta_list").innerHTML = html;
+  });
+}
