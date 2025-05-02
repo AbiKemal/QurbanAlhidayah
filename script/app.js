fbe172bdef8fb4041b5d8245e88a5b0ebb7eede1
AbiKemal <ssupendi@gmail.com>
2025-05-02 22:47:00 +0700

    Delete script/app.js

--- script/app.js
+++
@@ -1,6 +0,0 @@
-// Main application logic
-document.addEventListener('DOMContentLoaded', () => {
-  initGoogleCharts(() => {
-    loadSheetData(config.SHEETS.PESERTA_QURBAN, "peserta_list");
-  });
-});
