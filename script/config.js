f0f44262eb9d5cde9e1ee259ed21aa3e27c81ea0
AbiKemal <ssupendi@gmail.com>
2025-05-02 22:25:56 +0700

    Update config.js

--- script/config.js
+++ script/config.js
@@ -1,20 +1,26 @@
 const config = {
-  DATABASE: {
-    sheet_name: "DATABASE",
-    id: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRUl5hfyzjnnT1H-bN9Q5U5s4OXqnbWN0mDflynJnyA76bp8bMOYEMDvwnBY1UqwDJ8_P10jfOM0uOm/pub?gid=2142822523&single=true&output=csv"
+  SHEETS: {
+    DATABASE: {
+      sheet_name: "DATABASE",
+      id: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRUl5hfyzjnnT1H-bN9Q5U5s4OXqnbWN0mDflynJnyA76bp8bMOYEMDvwnBY1UqwDJ8_P10jfOM0uOm/pub?gid=2142822523&single=true&output=csv"
+    },
+    PESERTA_QURBAN: {
+      sheet_name: "PESERTA_QURBAN",
+      id: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRUl5hfyzjnnT1H-bN9Q5U5s4OXqnbWN0mDflynJnyA76bp8bMOYEMDvwnBY1UqwDJ8_P10jfOM0uOm/pub?gid=2033279411&single=true&output=csv",
+      gsheet_id: "1aNWfGYIqARaSmDXl0eqJ5Sy6htOgBgMTIZC1SlZA0vA",
+      default_query: "SELECT A, B"
+    },
+    LIST_BULAN: {
+      sheet_name: "LIST_BULAN",
+      id: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRUl5hfyzjnnT1H-bN9Q5U5s4OXqnbWN0mDflynJnyA76bp8bMOYEMDvwnBY1UqwDJ8_P10jfOM0uOm/pub?gid=2028680504&single=true&output=csv"
+    },
+    Admin: {
+      sheet_name: "list_Admin",
+      id: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRUl5hfyzjnnT1H-bN9Q5U5s4OXqnbWN0mDflynJnyA76bp8bMOYEMDvwnBY1UqwDJ8_P10jfOM0uOm/pub?gid=400129141&single=true&output=csv"
+    }
   },
-  PESERTA_QURBAN: {
-    sheet_name: "PESERTA_QURBAN",
-    id: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRUl5hfyzjnnT1H-bN9Q5U5s4OXqnbWN0mDflynJnyA76bp8bMOYEMDvwnBY1UqwDJ8_P10jfOM0uOm/pub?gid=2033279411&single=true&output=csv"
-  },
-  LIST_BULAN: {
-    sheet_name: "LIST_BULAN",
-    id: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRUl5hfyzjnnT1H-bN9Q5U5s4OXqnbWN0mDflynJnyA76bp8bMOYEMDvwnBY1UqwDJ8_P10jfOM0uOm/pub?gid=2028680504&single=true&output=csv"
-  },
-  Admin: {
-    sheet_name: "list_Admin",
-    id: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRUl5hfyzjnnT1H-bN9Q5U5s4OXqnbWN0mDflynJnyA76bp8bMOYEMDvwnBY1UqwDJ8_P10jfOM0uOm/pub?gid=400129141&single=true&output=csv"
+  API: {
+    BASE_URL: "https://docs.google.com/spreadsheets/d/",
+    GVIZ_ENDPOINT: "/gviz/tq"
   }
 };
-
-const gsheet = "1aNWfGYIqARaSmDXl0eqJ5Sy6htOgBgMTIZC1SlZA0vA";
