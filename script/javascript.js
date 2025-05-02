0ab4a7bab587c61559bdccf3d09211ef5db81997
AbiKemal <ssupendi@gmail.com>
2025-05-02 21:57:25 +0700

    Update javascript.js

--- script/javascript.js
+++ script/javascript.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Halaman index.html dimuat");
+
+  fetch("config.json")
+    .then(res => res.json())
+    .then(config => {
+      console.log("Data konfigurasi berhasil dimuat:", config);
+    })
+    .catch(err => console.error("Gagal memuat konfigurasi:", err));
+});
 
