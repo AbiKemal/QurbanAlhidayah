ec0588a6f7df869f55b6d8e2057d93aa6ff1d914
AbiKemal <ssupendi@gmail.com>
2025-05-02 23:42:04 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,28 +1,31 @@
-import config from './config.js';
+import config from "./config.js";
 
-google.charts.load('current', { packages: ['table'] });
+google.charts.load("current", { packages: ["table"] });
 google.charts.setOnLoadCallback(loadPesertaList);
 
 function loadPesertaList() {
-  const sheetInfo = config.sheets.PESERTA_QURBAN;
+  const sheetName = config.sheets.PESERTA_QURBAN.sheet_name;
   const queryString = encodeURIComponent("SELECT A, B");
 
-  const dataSourceUrl = `https://docs.google.com/spreadsheets/d/${config.gsheet}/gviz/tq?sheet=${sheetInfo.sheet_name}&headers=1&tq=${queryString}`;
+  const url = `https://docs.google.com/spreadsheets/d/${config.gsheet}/gviz/tq?sheet=${sheetName}&headers=1&tq=${queryString}`;
 
-  const query = new google.visualization.Query(dataSourceUrl);
-  query.send((response) => {
+  const query = new google.visualization.Query(url);
+  query.send(function (response) {
     if (response.isError()) {
-      document.getElementById("peserta_list").innerHTML = "Gagal memuat data peserta.";
+      document.getElementById("peserta_list").innerHTML =
+        "Gagal memuat data peserta.";
       return;
     }
 
     const data = response.getDataTable();
-    let html = '<ul>';
+    let html = "<ul>";
     for (let i = 0; i < data.getNumberOfRows(); i++) {
-      const nama = data.getValue(i, 1); // Kolom B
-      html += `<li><a href="view.html?nama=${encodeURIComponent(nama)}">${data.getValue(i, 0)} - ${nama}</a></li>`;
+      const nama = data.getValue(i, 1);
+      html += `<li><a href="view.html?nama=${encodeURIComponent(
+        nama
+      )}">${data.getValue(i, 0)} - ${nama}</a></li>`;
     }
-    html += '</ul>';
+    html += "</ul>";
     document.getElementById("peserta_list").innerHTML = html;
   });
 }
